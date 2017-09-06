b521c65a8e55c6c24e36586d4d90de576e9a2a99
Marvin Duran <marvineduran@gmail.com>
2017-09-06 10:03:44 -0600

    Adding Required Node Modules:
    * chai                                         (Assert Library)
    * chai-http                                (Extension to assert http request)
    * mocha                                    (Test Enviroment)
    * moment                                 (Handle Dates better)
    * body-parser                           (Fetch request data from body)
    * express                                    (Framework to build the web service)
    * mariasql                                  (Methods to access MariaDB, chosen DB)
    * passport                                  (Chosen Authentication System)
    * passport-jwt                           (JWT Authentication Method for passport)
    * jsonwebtoken                        (To generate JWTs for authentication)
    
    Adding typings to node modules

---
+++ Authentication/JWT.ts
@@ -0,0 +1,55 @@
+﻿//Node Modules
+import * as passport from 'passport';
+import * as passportJwt from 'passport-jwt';
+import * as jsonwebtoken from 'jsonwebtoken';
+import * as bodyParser from 'body-parser';
+
+//Namespaces and Functions
+import c_config from '../config';
+import credential from "../Models/credential"
+
+export default class JWT {
+    static setStrategy(app): void {
+        app.use(passport.initialize());
+        app.use(bodyParser.urlencoded({ extended: true }));
+        app.use(bodyParser.json());
+
+        passport.use(new passportJwt.Strategy(c_config.JWT_VERY_OPTION, (jwt_payload, next) => next(null, jwt_payload)));
+    };
+
+    static setLoginRoute(app): void {
+        app.post("/login",
+            //passport.authenticate('jwt', { successRedirect: '/', failureRedirect: '/login', failureFlash: "Please Log in to see this content", session: false}),
+            async (req: any, res: any) => {
+
+                if (req.body.name == undefined && req.body.password == undefined) {
+                    res.status(401).json({ message: "No credential Provided" });
+                }
+
+                let users = await credential.getAllCredentials();
+                let user = users.find(user => { console.dir(user); return user.isUser(req.body.name, req.body.password) });
+
+                if (user != undefined) {
+                    let payload = { id: user.id, role: user.role };
+                    let token = jsonwebtoken.sign(payload, c_config.SECRET, c_config.JWT_SING_OPTIONS);
+                    res.json({ response: "OK", token: token });
+                } else {
+                    res.status(401).json({ message: "password or username is Incorrect", response: "failed" });
+                }
+            });
+    };
+
+    static authorizeTo(selfAuthorize: boolean, ...authorizedRoles: number[]) {
+        return function (req, res, next) {
+            console.dir(req.user);
+            const currentUserRole = Number.parseInt(req.user.role);
+            let isSelfReq = selfAuthorize && req.user.id === req.param(`id`, -1);
+            if (!isSelfReq || !authorizedRoles.some(x => x == currentUserRole)) {
+                res.status(403);
+                res.send('Not permitted');
+                return;
+            }
+            next();
+        }
+    }
+}
---
+++ DBManagers/mariaDB.ts
@@ -0,0 +1,16 @@
+﻿import c_config from '../config';
+import * as MariaClient from 'mariaSQL';
+
+export default class MariaDBUtils {
+    static getConnection() {
+        /// <summary>
+        /// returns a conection to MariaDB with the given configuration
+        /// </summary>
+        /// <returns type="">MariaDB Conection</returns>
+        var client = new MariaClient();
+        client.connect(c_config.MARIADB_CONNECTION_INFO);
+        return (client);
+    }
+
+}
+
